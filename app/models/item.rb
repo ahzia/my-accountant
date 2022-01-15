1766f5fc15139af5371f31dfc00252908cbac9b4
ahzia <ah.ziayosfi@gmail.com>
2022-01-15 19:39:11 +0430

    add validation to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   belongs_to :author, class_name: :User
   has_many :joins, foreign_key: 'items_id', class_name: 'Join'
   has_many :catagories, through: :joins # Edit :needs to be plural same as the has_many relationship
+
+  validates :name, :amount, presence: true, length: { maximum: 255 }
+  validates :amount, numerically: { greater_than: 0 }
 end
